7e49cebb2584ca2e947b9e660a74d2b16190208e
吉村幸翼 <koshuke0217@icloud.com>
2018-02-04 12:05:08 +0900

    Edit for make group

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
 
   devise_for :users
-root  'messages#index'
-resource :user, only: [:edit, :update]
+  root  'messages#index'
+  resource :user, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+
+  end
 
 end
