0cc596876c6d6777822624d91449ccaa86fed11d
吉村幸翼 <koshuke0217@icloud.com>
2018-02-10 11:57:22 +0900

    Edit message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,15 +2,20 @@ $(function(){
   function buildHTML(message){
 //functionによって関数の宣言 buildHTMlは関数の名前 messageは引数
 //function 関数名 (引数){関数を定義する文}
-    var html = `.upper-message
-                    .upper-message__user-name
-                      ${message.user_name}
-                    .upper-message__date
-                      ${message.created_at}
-                  .lower-meesage
-                    - if message.content.present?
-                      %p.lower-message__content
-                        ${message.content}`
+    var html = `<div class="upper-message">
+                  <div class="upper-message__user-name">
+                  ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                  ${message.created_at}
+                  </div>
+                </div>
+                  <div class="lower-message">
+                  <p class="lower-message__content">
+                  ${message.content}
+                  </p>
+                </div>`
+
     return html;
 //htmlという変数を作った
 //代入するものは複数行に渡るのでテンプレートリテラル記法によって書く
@@ -23,7 +28,7 @@ $(function(){
 //FormDataオブジェクトはフォームのデータの送信に使用できる
 //ここではフォームの情報を取得するのに使う
 //イベントで設定したfunction内でthisを利用した場合はそのthisはイベントが発生したDOM要素を示す ここではnew_messageというIDがついたフォームの情報を取得している
-    var href = window.location.href + '/messages'
+    var href = window.location.href
 //hrefという変数を作った
 //windowオブジェクトとは画面上に表示されている全てのオブジェクトの親となるオブジェクト
 //locationオブジェクトはwindowオブジェクションの一部であり、window.locationプロパティを通じてアクセスできる。locationオブジェクトのプロパティの一つであるhrefはurl全体という意味
@@ -47,12 +52,13 @@ $(function(){
 //即時関数の第一引数になっているdataとは？サーバーから返ってくるデータのこと（今回はjbuilderで作成したcreate.json.jbuilderのデータのこと）
       var html = buildHTML(data);
 //htmlという変数にcreate.json.jbuilderのデータを代入する
-      $('.message').append(html)
+      $('.messages').append(html)
 //appendメソッドとは？→$(‘セレクタ’).append(‘追加するもの’);
 //val()はvalue属性を取得、操作することができる
 //引数ありで呼ぶことで中身をその値に帰ることができる
     })
    .fail(function(){
+//サーバーエラー(通信に失敗した時)fail関数が呼ばれる
       alert('error');
     })
   })
