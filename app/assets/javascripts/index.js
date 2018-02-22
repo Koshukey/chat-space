e926720fb8041fc520ff3c251c91fc52282f4d7a
吉村幸翼 <koshuke0217@icloud.com>
2018-02-22 19:30:12 +0900

    インクルメンタルサーチのため

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,43 @@
+$(function() {
+
+  var user_search_list = $(".chat-group-form__field--right");
+
+  function appendUser(user) {
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
+                </div>
+               `
+    user_search_list.append(html);
+  }
+
+  $(function() {
+//$(function() {などはjsの即時関数 即時関数→、関数を定義すると同時に実行するための構文
+    $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+//valueメソッドでフォームの値を取得
+      var href = window.location.href
+
+      console.log(input);
+
+      $.ajax({
+        type: 'GET',
+        url: href,
+        data: { keyword: input },
+        dataType: 'json'
+      })
+
+      .done(function(data) {
+        var html = appendUser(data);
+        $('chat-group-form__field--right').append(html);
+      })
+
+      .fail(function(){
+        alert('error');
+      })
+
+    })
+
+  });
+
+});
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,12 @@ class UsersController < ApplicationController
     end
   end
 
-  def search
+  def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
     end
-  end
-
-
-
   end
 
   private
