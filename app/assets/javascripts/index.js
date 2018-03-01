8c9d404f8c56c0f4bb887f2c6aa2344833b1eca8
Kosukey <kosukeyoshimura@yoshimuramiyukitsubasanoMacBook.local>
2018-03-01 10:04:15 +0900

    無駄なネストをなくした

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,17 +1,19 @@
 $(function() {
 
   function appendUser(user) {
-    var html = `<div class="chat-group-form__field--right--search">
+    var html = `
+                <div class="chat-group-form__field--right--search">
                   <div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${user.name}</p>
                       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                   </div>
                </div>
+
                `
    return html;
   };
 
-  $(function() {
+
 //$(function() {などはjsの即時関数 即時関数→、関数を定義すると同時に実行するための構文
     $("#user-search-field").on("keyup", function() {
       var input = $("#user-search-field").val();
@@ -42,7 +44,5 @@ $(function() {
       });
 
     });
-
-  });
 
 });
