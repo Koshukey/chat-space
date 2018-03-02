acdd8b143de6efc8f575d08352b81318644868ed
Kosukey <kosukeyoshimura@yoshimuramiyukitsubasanoMacBook.local>
2018-03-02 12:37:21 +0900

    チャットメンバーというviewの追加と追加ボタンでチャットメンバーが追加されるように編集
    _form.html.hamlの@usersが最初空なのでそれでエラーが出る

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,11 +8,9 @@ $(function() {
                       <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                   </div>
                </div>
-
                `
    return html;
   };
-
 
 //$(function() {などはjsの即時関数 即時関数→、関数を定義すると同時に実行するための構文
     $("#user-search-field").on("keyup", function() {
@@ -34,6 +32,7 @@ $(function() {
         if (users.length !== 0) {
           users.forEach(function(user){
             var html = appendUser(user);
+            console.log(html)
             $(".user-search-result").append(html);
           });
         }
@@ -46,3 +45,40 @@ $(function() {
     });
 
 });
+
+  function clickHTML(user){
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value="${user.attr("data-user-id")}">
+                  <p class='chat-group-
+                  user__name'>${user.attr("data-user-name")}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+               </div>`
+    return html;
+   };
+//attrメソッドによって引数に指定した属性の値を取得することができる
+//HTML5ではdata-*="value"の形式で属性名にプライベートな値を設定できるカスタムデータ属性の仕様と、そのカスタムデータ属性にJavaScriptからアクセスするAPIが定義された
+
+
+
+  $(document).on("click",".user-search-add", function() {
+//追加ボタンが押された時
+    $input = $(this);
+    var add_user_html = clickHTML($input);
+    $("#search-users").append( add_user_html);
+//#search-usersの下にhtmlを追加
+      $input.parent().remove();
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        dataType: 'json',
+      })
+      .done(function(data){
+      })
+      .fail(function(){
+         alert('追加に失敗しました');
+      })
+  });
+
+
+
+
