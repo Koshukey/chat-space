297ba1078ce95608feae37e108cb3a836770c982
Koshukey <kosukeyoshimura@yoshimuramiyukitsubasanoMacBook.local>
2018-04-03 15:20:53 +0900

    本番でも画像投稿できるようにした

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,7 @@ namespace :deploy do
     invoke 'unicorn:restart'
   end
 end
+set :linked_files, %w{ config/secrets.yml }
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
